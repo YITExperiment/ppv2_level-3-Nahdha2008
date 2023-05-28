91c4712dd978a99ef2af80460da4cc9202e06451
Nahdha2008 <134832164+Nahdha2008@users.noreply.github.com>
2023-05-28 12:44:10 +0530

    Add files via upload

---
+++ age_weight_height.py
@@ -0,0 +1,19 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> age=12
+>>> print(age)
+12
+>>> 
+>>> Weight=40
+>>> print(Weight)
+40
+>>> 
+>>> Height=157
+... print(Height)
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+>>> Height=157
+>>> print(Height)
+157
